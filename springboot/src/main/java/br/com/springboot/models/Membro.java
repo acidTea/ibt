33076482f642a1b130c23617b58105fc96cde86f
Andre Landim <landim@live.in>
2023-04-28 10:15:16 -0300

    teste

--- springboot/src/main/java/br/com/springboot/models/Membro.java
+++ springboot/src/main/java/br/com/springboot/models/Membro.java
@@ -9,7 +9,6 @@ import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIdentityReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 import jakarta.persistence.Column;
